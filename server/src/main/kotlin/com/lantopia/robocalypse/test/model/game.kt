0487b2c69162cdeff2e3ca74a30e8ed33231cc9e
Brent Smith <brent.smith@redspace.com>
2015-08-26 21:47:35 -0300

    Removed duplicate models

--- server/src/main/kotlin/com/lantopia/robocalypse/test/model/game.kt
+++
@@ -1,5 +0,0 @@
-package com.lantopia.robocalypse.test.model
-
-public data class Player(val name : String, val robots : Set<Robot>)
-
-public data class Game(val world : World, val players : Set<Player>)
